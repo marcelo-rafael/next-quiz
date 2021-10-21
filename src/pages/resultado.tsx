b6f631b94f3bbd9980c88d06e7d04e96b5367115
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2021-10-21 08:26:23 -0300

    Componente resultado

--- src/pages/resultado.tsx
+++ src/pages/resultado.tsx
@@ -1,7 +1,21 @@
+import { useRouter } from 'next/router'
+
+import styles from '../styles/Resultado.module.css'
+
 export default function resultado() {
+  const router = useRouter()
+
+  const total = +router.query.total
+  const certas = +router.query.certas
+  const percentual = Math.round((certas / total) * 100)
+
+
   return (
-    <div>
-      <h1>Resultado</h1>
+    <div className={styles.resultado}>
+      <h1>Resultado final</h1>
+      <div>{total}</div>
+      <div>{certas}</div>
+      <div>{`${percentual}%`}</div>
     </div>
   )
 }
